014e3fe41519427815332fc7ff800f4670f9ec0f
Leonardo Porto <leonardo.porto.souza@gmail.com>
2021-05-26 23:14:55 -0300

    ci: heroku env port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const { logger } = require('./helpers/logger.helper')
 
-app.listen(process.env.NODE_PORT || 8080)
+app.listen(process.env.PORT || 8080)
 
-logger.info(`Server listening on port ${process.env.NODE_PORT || 8080}`)
+logger.info(`Server listening on port ${process.env.PORT || 8080}`)
